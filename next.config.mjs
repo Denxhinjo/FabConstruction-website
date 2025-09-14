d2ed97186737f5acf065831ab5aa5d5f6b7de2f0
Denis M. Xhabrahimi <83427579+Denxhinjo@users.noreply.github.com>
2025-09-14 13:02:26 +0200

    Add Next.js configuration with image domains

---
+++ next.config.mjs
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ["cdn.sanity.io"]
+  },
+  experimental: {
+    optimizePackageImports: ["clsx"]
+  }
+};
+export default nextConfig;
